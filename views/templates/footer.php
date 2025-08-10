84d5586ec86652d0a5685365472f6683a0614e2f
Christopher <morochito344@gmail.com>
2025-08-09 22:38:20 -0500

    finalizando footer

--- views/templates/footer.php
+++ views/templates/footer.php
@@ -1,5 +1,32 @@
-    <footer class="footer">
-        <p class="footer__texto">
-            Front End Store by Christopher Revelo - Todos los derechos reservados 2024
-        </p>
-    </footer>
+<footer class="footer">
+
+
+    <nav class="menu-social">
+            <p class="texto-especial">Joya de los Sachas, Orellana - Ecuador</p>
+            <a class="menu-social__enlace" rel="noopener noreferrer" target="_blank" href="https://facebook.com/alkimiaboutique.ec">
+                <span class="menu-social__ocultar">Facebook</span>
+            </a> 
+            <a class="menu-social__enlace" rel="noopener noreferrer" target="_blank" href="https://twitter.com">
+                <span class="menu-social__ocultar">Twitter</span>
+            </a> 
+            <a class="menu-social__enlace" rel="noopener noreferrer" target="_blank" href="https://youtube.com">
+                <span class="menu-social__ocultar">YouTube</span>
+            </a> 
+            <a class="menu-social__enlace" rel="noopener noreferrer" target="_blank" href="https://instagram.com/storealkimia">
+                <span class="menu-social__ocultar">Instagram</span>
+            </a> 
+            <a class="menu-social__enlace" rel="noopener noreferrer" target="_blank" href="https://tiktok.com/@alkimia.fashion">
+                <span class="menu-social__ocultar">Tiktok</span>
+            </a> 
+            <a class="menu-social__enlace" rel="noopener noreferrer" target="_blank" href="https://github.com/Alexander3456R">
+                <span class="menu-social__ocultar">GitHub</span>
+            </a>
+    </nav>
+
+    <p class="footer__copyright">
+        Alkimia Fashion Boutique
+        <span class="footer__copyright--regular">
+            - Todos los derechos reservados <?php echo date('Y'); ?>
+        </span>
+    </p>
+</footer>
